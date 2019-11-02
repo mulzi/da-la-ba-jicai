81a0b6afa7e2f7becd4ef36de4bc3e7efe251470
mulizi <910881441@qq.com>
2019-11-02 12:02:20 +0800

    11月1日提交

---
+++ components/works/Comment.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="liList">
+    <div class="l_img">
+      <img src="@/assets/img/0123.jpg" alt="">
+    </div>
+    <div class="r_Li_list">
+      <div class="topTit">
+        <span>用户名称：</span>撒大声地阿萨德阿萨德啊实打实大啊实打实大阿萨德啊
+      </div>
+      <div class="time">
+        <div class="l_time">
+          今天 11:00
+        </div>
+        <div class="r_click_Reply">
+          回复
+        </div>
+      </div>
+      <div class="n_list">
+        <div class="n_liList">
+          <div class="l_img">
+            <img src="@/assets/img/0123.jpg" alt="">
+          </div>
+          <div class="r_Li_list">
+            <div class="topTit">
+              <span>用户名称：</span><em>回复<i>@用户名称</i></em> 撒大声地阿萨德阿萨德啊实打实大啊实打实大阿萨德啊
+            </div>
+            <div class="time">
+              <div class="l_time">
+                今天 11:00
+              </div>
+              <div class="r_click_Reply">
+                回复
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="n_liList">
+          <div class="l_img">
+            <img src="@/assets/img/0123.jpg" alt="">
+          </div>
+          <div class="r_Li_list">
+            <div class="topTit">
+              <span>用户名称：</span><em>回复<i>@用户名称</i></em> 撒大声地阿萨德阿萨德啊实打实大啊实打实大阿萨德啊
+            </div>
+            <div class="time">
+              <div class="l_time">
+                今天 11:00
+              </div>
+              <div class="r_click_Reply">
+                回复
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="n_liList">
+          <div class="l_img">
+            <img src="@/assets/img/0123.jpg" alt="">
+          </div>
+          <div class="r_Li_list">
+            <div class="topTit">
+              <span>用户名称：</span><em>回复<i>@用户名称</i></em> 撒大声地阿萨德阿萨德啊实打实大啊实打实大阿萨德啊
+            </div>
+            <div class="time">
+              <div class="l_time">
+                今天 11:00
+              </div>
+              <div class="r_click_Reply">
+                回复
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="n_liList">
+          <div class="l_img">
+            <img src="@/assets/img/0123.jpg" alt="">
+          </div>
+          <div class="r_Li_list">
+            <div class="topTit">
+              <span>用户名称：</span><em>回复<i>@用户名称</i></em> 撒大声地阿萨德阿萨德啊实打实大啊实打实大阿萨德啊
+            </div>
+            <div class="time">
+              <div class="l_time">
+                今天 11:00
+              </div>
+              <div class="r_click_Reply">
+                回复
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="spread">
+          <span>展开查看更多9条评论 <i class="el-icon-arrow-down" /></span>
+        </div>
+        <div class="fewer">
+          <span>
+            收起更多评论 <i class="el-icon-arrow-up" />
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment'
+}
+</script>
+
+<style scoped lang="scss">
+    .liList{
+        width: 100%;
+        display: flex;
+        display: -ms-flex;
+        margin-bottom: 30px;
+        >.l_img{
+            width: 40px;
+            height: 40px;
+            img{
+                @include img;
+            }
+        }
+        >.r_Li_list{
+            flex: 1;
+            margin-left: 10px;
+            overflow: hidden;
+            >.topTit{
+                width: 100%;
+                font-size:16px ;
+                color: #666666;
+                line-height: 24px;
+                min-height: 48px;
+                span{
+                    color: $redColor;
+                }
+            }
+            >.time{
+                overflow: hidden;
+                margin-top: 6px;
+                .l_time{
+                    float: left;
+                    color: #999999;
+                    font-size: 16px;
+                }
+                .r_click_Reply{
+                    float: right;
+                    color: #999999;
+                    font-size: 16px;
+                    cursor: pointer;
+                }
+            }
+            >.n_list{
+                width: 100%;
+                background: #f2f2f2;
+                margin-top: 10px;
+                overflow: hidden;
+                .n_liList{
+                    width: 97%;
+                    margin: 10px auto ;
+                    border-bottom: 1px solid #d9d9d9;
+                    display: flex;
+                    display: -ms-flex;
+                    .l_img{
+                        width: 40px;
+                        height: 40px;
+                        img{
+                            @include img;
+                        }
+                    }
+                    .r_Li_list{
+                        flex: 1;
+                        overflow: hidden;
+                        -ms-flex: 1;
+                        margin-left: 10px;
+                        .topTit{
+                            width: 100%;
+                            font-size: 15px;
+                            color: #666666;
+                            line-height: 22px;
+                            min-height: 44px;
+                            span{
+                                color: $redColor;
+
+                            }
+                            em{
+                                i{
+                                    color: $redColor;
+                                }
+                            }
+                        }
+                        >.time{
+                            overflow: hidden;
+                            margin-top: 6px;
+                            margin-bottom: 10px;
+                            .l_time{
+                                float: left;
+                                color: #999999;
+                                font-size: 16px;
+                            }
+                            .r_click_Reply{
+                                float: right;
+                                color: #999999;
+                                font-size: 16px;
+                                cursor: pointer;
+                            }
+                        }
+
+                    }
+                }
+                .spread{
+                    width: 100%;
+                    height:40px ;
+                    line-height: 40px;
+                    span{
+                        color: $redColor;
+                        font-size: 16px;
+                        margin-left: 10px;
+                        cursor: pointer;
+
+                    }
+                }
+                .fewer{
+                    width: 100%;
+                    height:40px ;
+                    line-height: 40px;
+                    text-align: right;
+                    span{
+                        color: $redColor;
+                        font-size: 16px;
+                        margin-right: 10px;
+                        cursor: pointer;
+                    }
+                }
+            }
+        }
+    }
+</style>
--- pages/works/worksPage/_id.vue
+++ pages/works/worksPage/_id.vue
@@ -82,6 +82,9 @@
                 <span>评&nbsp;&nbsp;论</span>
               </div>
             </div>
+            <div class="b_list_Box">
+              <comment />
+            </div>
           </el-row>
         </el-row>
         <el-row class="r_content_box">
@@ -173,16 +176,347 @@
           </el-row>
         </el-row>
       </el-row>
+      <el-row class="fxiedComment">
+        <div class="BG">
+        </div>
+        <div class="comment">
+          <div class="bodyContent">
+            <div class="close">
+              <span class="el-icon-close"></span>
+            </div>
+            <div class="name">
+              你想对 <span>用户名字</span>说什么
+            </div>
+            <div class="text">
+              <textarea placeholder="写点什么吧"/>
+            </div>
+            <div class="sub">
+                 <span>提交</span>
+            </div>
+          </div>
+        </div>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
+import Comment from '@/components/works/Comment'
 export default {
+  components: { Comment },
+
   layout: 'main'
 }
 </script>
 
 <style scoped lang="scss">
+  .works_content_box{
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    display: -ms-flex;
+    .l_content_box{
+      overflow: hidden;
+      flex: 1;
+      -ms-flex: 1;
 
+      .article_top{
+        width: 100%;
+        border-bottom: 1px solid $borderE7;
+        background: #ffffff;
+        .nameTit_box{
+          width: 92%;
+          margin: 30px auto 0;
+          display: flex;
+          display: -ms-flex;
+          justify-items: center;
+          align-items: center;
+          .name{
+            flex: 1;
+            -ms-flex: 1;
+            height: 24px;
+            line-height: 24px;
+            font-size: 22px;
+            color: #333333;
+            margin-right: 40px;
+            @include over;
+          }
+          .span{
+            cursor: pointer;
+            height: 40px;
+            line-height: 40px;
+            text-align: center;
+            width: 114px;
+            background: #FFD100;
+            border-radius: 4px;
+
+          }
+        }
+        .Top_timeBox{
+          width: 92%;
+          margin:30px auto 10px;
+          overflow: hidden;
+          .L_time{
+            float: left;
+            width: 200px;
+            font-size: 14px;
+            color: #999999;
+            height: 16px;
+            line-height: 16px;
+          }
+          .number_icon{
+            float: right;
+            height: 16px;
+            line-height: 16px;
+            color: #999999;
+            font-size: 14px;
+            i{
+              margin: 0 8px;
+            }
+          }
+        }
+      }
+      .content_box{
+        width: 100%;
+        margin: 0 auto ;
+        padding-top: 40px;
+        background: #ffffff;
+        .tit{
+          width: 92%;
+          margin: 0 auto;
+          text-align: center;
+          color: #333333;
+          font-size:18px ;
+        }
+        .content_c{
+          width: 92%;
+          margin: 20px auto 40px;
+          p{
+            width: 100%;
+            text-indent: 2em;
+            color:#333333 ;
+            font-size: 16px;
+            line-height: 28px;
+            margin-bottom: 20px;
+            img{
+              max-width: 94%;
+              display: block;
+              margin: 0 auto;
+            }
+          }
+        }
+        .content_b{
+          width: 92%;
+          margin: 20px auto 0;
+          p{
+            width: 100%;
+            text-indent: 2em;
+            color:#333333 ;
+            font-size: 16px;
+            line-height: 28px;
+          }
+        }
+        .praise{
+          width: 100%;
+          margin: 40px auto;
+          text-align: center;
+          .circle{
+            margin: 0 auto;
+            width: 60px;
+            height: 60px;
+            line-height: 60px;
+            text-align: center;
+            color: #ffffff;
+            border-radius: 50%;
+            background: #d9d9d9;
+            cursor: pointer;
+            &.active{
+              background: #f88685;
+            }
+            i{
+              font-size: 24px;
+            }
+          }
+          span{
+            display: inline-block;
+            margin-top: 10px;
+            font-size:14px;
+            color: #333333;
+          }
+        }
+      }
+      .comment_box{
+        background: #ffffff;
+        margin: 40px auto 0;
+        width: 100%;
+        overflow: hidden;
+        .top_form{
+          width: 100%;
+          margin-top: 30px ;
+          .text_Box{
+            width: 94%;
+            margin: 0 auto;
+            border: 1px solid $borderE7;
+            border-radius: 6px;
+            textarea{
+              width: 94%;
+              margin: 20px auto;
+              height: 96px;
+              line-height: 24px;
+              font-size: 16px;
+              color: #333333;
+              display: block;
+              resize: none;
+              outline: none;
+            }
+          }
+          .sub{
+            width: 94%;
+            margin: 20px auto;
+            overflow: hidden;
+            span{
+              display: block;
+              float: right;
+              width: 100px;
+              height: 40px;
+              line-height: 40px;
+              text-align: center;
+              color: #ffffff;
+              background: $redColor;
+              cursor: pointer;
+            }
+          }
+        }
+        .b_list_Box{
+          width: 94%;
+          margin: 30px auto ;
+          overflow: hidden;
+
+        }
+      }
+    }
+    .r_content_box{
+      width:326px ;
+      margin-left: 30px;
+      .top_img_box{
+        background: #ffffff;
+        width: 100%;
+        overflow: hidden;
+        .img{
+          width: 86px;
+          height: 86px;
+          border-radius: 50%;
+          overflow: hidden;
+          margin: 30px auto 0;
+          img{
+            @include img;
+          }
+        }
+        .name{
+          width: 92%;
+          margin: 20px auto 0 ;
+          height: 20px;
+          line-height: 20px;
+          font-size: 18px;
+          color: #333333;
+          text-align: center;
+          @include over;
+        }
+        .adds_a{
+          width: 92%;
+          margin: 10px auto 0 ;
+          height: 20px;
+          line-height: 20px;
+          font-size: 15px;
+          color: #999999;
+          text-align: center;
+          @include over;
+        }
+        .click{
+          display: block;
+          margin: 20px auto ;
+          width: 140px;
+          height: 40px;
+          line-height: 40px;
+          text-align: center;
+          font-size: 14px;
+          color: #333333;
+          background: #FFD100;
+          border-radius: 4px;
+
+        }
+      }
+      .selection{
+        width: 100%;
+        background: #ffffff;
+        margin: 10px auto;
+        .tit{
+          height: 50px;
+          line-height: 50px;
+          text-indent: 20px;
+          color: #333333;
+          font-size: 18px;
+          border-bottom: 1px solid $borderE7;
+
+        }
+        .list_a{
+          margin: 10px auto 0;
+          width: 100%;
+          border-bottom: 1px solid $borderE7;
+          a{
+            display: flex;
+            display: -ms-flex;
+            justify-items: center;
+            align-items: center;
+            &:last-child{
+              margin-bottom: 10px;
+            }
+            &:hover{
+              span{
+                color: $redColor;
+              }
+              em{
+                color: $redColor;
+              }
+            }
+            span{
+              flex: 1;
+              -ms-flex: 1;
+              margin-left: 20px;
+              font-size: 16px;
+              color: #333333;
+              @include over;
+              height: 36px;
+              line-height: 36px;
+              display: inline-block;
+            }
+            em{
+              width: 70px;
+              margin-left: 10px;
+              margin-right: 20px;
+              color: #999999;
+              font-size: 14px;
+              text-align: right;
+              display: inline-block;
+            }
+          }
+        }
+        .moreList{
+          width: 100%;
+          text-align: center;
+          a{
+            display: inline-block;
+            margin: 20px auto;
+            color: #333333;
+            font-size: 16px;
+            &:hover{
+              color: $redColor;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
+
+\
