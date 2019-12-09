c3f5bddf090bb7980b16e7ca7f533ebeab680784
mulzi <910881441@qq.com>
2019-12-09 23:32:53 +0800

    路由跳转方法

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,19 @@
 export default {
   mode: 'universal',
   router: {
-    middleware: 'isLogin'
+    middleware: 'isLogin',
+    extendRoutes (routes, resolve) {
+      routes.push({
+        name: 'custom',
+        path: '/works/ss/dd',
+        component: resolve(__dirname, 'pages/works/index')
+      })
+      routes.push({
+        name: 'ss',
+        path: '/works/:code/:code/de',
+        component: resolve(__dirname, 'pages/works/index')
+      })
+    }
   },
   /*
     ** Headers of the page
